2824f310d49c91519f4c8040c3b834839bee2ddb
Ketan <ketanbammidi@gmail.com>
2026-01-29 13:11:51 +0000

    base environment config with abstract classes

---
+++ environments/base_env.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+import gymnasium as gym
+
+class BaseEnv(gym, Env, ABC):
+    # abstract base class for all sentinelRL environments
+
+    metadata = {"render_modes: ": ["human", None]}
+
+    def __init__(self, render_mode = None):
+        super().__init__()
+        self.render_mode = render_mode
+    
+    @abstractmethod
+    def reset(self, *, seed=None, options=None):
+        pass
+
+    @abstractmethod
+    def step(self, action):
+        pass
+
+    @abstractmethod
+    def inject_drift(self, **kwargs):
+        # apply specs or reward drift, called explicitly during eval or curriculum training
+        pass
